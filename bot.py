5216b9fb5edec1a7706974f81310a0998c7edbf7
Maksim Pozhidaev <maxoovka1@gmail.com>
2024-10-24 16:29:04 +0300

    chore: huge refactor

--- bot.py
+++ bot.py
@@ -1,31 +1,26 @@
 import asyncio
-import secrets
 import os
+import secrets
 import sys
 
-from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.filters import Command
-from aiogram.types import Message
-from aiogram.utils.deep_linking import create_start_link
+import aiogram
 
 import database
 
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-if TELEGRAM_BOT_TOKEN is None:
+if not TELEGRAM_BOT_TOKEN:
     sys.exit("TELEGRAM_BOT_TOKEN is not set")
 
-dispatcher = Dispatcher()
+dispatcher = aiogram.Dispatcher()
 
 
-def generate_unique_id() -> str:
-    return secrets.token_urlsafe(8)
+def generate_unique_id(number_of_bytes: int = 8) -> str:
+    return secrets.token_hex(number_of_bytes)
 
 
-def get_command_arguments(text: str) -> list[str]:
-    return text.split()[1:]
+def get_command_arguments(text: str, start: int = 1) -> list[str]:
+    return text.split()[start:]
 
 
 def is_admin(user_id: int) -> bool:
@@ -33,19 +28,18 @@ def is_admin(user_id: int) -> bool:
     return admins_user_ids and user_id in admins_user_ids
 
 
-@dispatcher.message(Command("start", "get"))
-async def start_command_handler(message: Message) -> None:
+@dispatcher.message(aiogram.filters.Command("start", "get"))
+async def start_command_handler(message: aiogram.types.Message) -> None:
     arguments = get_command_arguments(message.text)
     if len(arguments) < 1:
         return
 
-    audio_query_result = database.query_get_audio(arguments[0])
-    if audio_query_result:
+    if audio_query_result := database.query_get_audio(arguments[0]):
         await message.reply_audio(audio_query_result["telegram_file_id"])
 
 
-@dispatcher.message(Command("remove"))
-async def remove_command_handler(message: Message) -> None:
+@dispatcher.message(aiogram.filters.Command("remove"))
+async def remove_command_handler(message: aiogram.types.Message) -> None:
     if not is_admin(message.from_user.id):
         return
 
@@ -54,32 +48,35 @@ async def remove_command_handler(message: Message) -> None:
         return
 
     generated_id = arguments[0]
-
     if database.query_remove_audio(generated_id):
         await message.reply(f"removed {generated_id}")
 
 
 @dispatcher.message()
-async def audio_message_handler(message: Message) -> None:
+async def audio_message_handler(message: aiogram.types.Message) -> None:
     if not is_admin(message.from_user.id) or not message.audio:
         return
 
     generated_id = generate_unique_id()
-
-    database.query_create_audio(generated_id, message.audio.file_id)
-    await message.reply(
-        await create_start_link(message.bot, generated_id),
-    )
+    link = await aiogram.utils.deep_linking.create_start_link(message.bot, generated_id)
+    if database.query_create_audio(generated_id, message.audio.file_id):
+        await message.reply(link)
 
 
 async def main() -> None:
-    bot = Bot(
+    bot = aiogram.Bot(
         token=TELEGRAM_BOT_TOKEN,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+        default=aiogram.client.default.DefaultBotProperties(
+            parse_mode=aiogram.enums.ParseMode.HTML
+        ),
     )
-
     await dispatcher.start_polling(bot)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        pass
--- database.py
+++ database.py
@@ -7,8 +7,7 @@ import sqlite3
 @contextmanager
 def __database_context():
     connection = sqlite3.connect("database.sqlite")
-    connection.row_factory = __row_factory
-
+    connection.row_factory = sqlite3.Row
     try:
         yield connection
         connection.commit()
@@ -18,43 +17,33 @@ def __database_context():
         connection.close()
 
 
-def __row_factory(cursor: sqlite3.Cursor, row) -> dict:
-    fields = [column[0] for column in cursor.description]
-    return {key: value for key, value in zip(fields, row)}
-
-
-def query_get_admins() -> list[int]:
+def query_get_admins() -> list[int] | None:
     with __database_context() as connection:
-        query_result = connection.execute(
-            "SELECT user_id FROM admin",
-        ).fetchall()
-
+        query_result = connection.execute("SELECT user_id FROM admin").fetchall()
         return [result["user_id"] for result in query_result]
 
 
-def query_get_audio(generated_id: str) -> dict:
+def query_get_audio(generated_id: str) -> dict | None:
     with __database_context() as connection:
         query_result = connection.execute(
             "SELECT generated_id, telegram_file_id FROM audio WHERE generated_id = ?",
             [generated_id],
         ).fetchone()
-
         return query_result
 
 
-def query_remove_audio(generated_id: str) -> bool:
+def query_remove_audio(generated_id: str) -> bool | None:
     with __database_context() as connection:
         query_result = connection.execute(
-            "DELETE FROM audio WHERE generated_id = ?",
-            [generated_id],
+            "DELETE FROM audio WHERE generated_id = ?", [generated_id]
         )
-
         return query_result.rowcount > 0
 
 
-def query_create_audio(generated_id: str, telegram_file_id: str) -> None:
+def query_create_audio(generated_id: str, telegram_file_id: str) -> bool | None:
     with __database_context() as connection:
         connection.execute(
             "INSERT INTO audio (generated_id, telegram_file_id) VALUES (?, ?)",
             [generated_id, telegram_file_id],
         )
+        return True
